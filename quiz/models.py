4ac58b048d56b82b5edde4caac62e9b387805316
frukcharkrit <chakrit.ph.66@ubu.ac.th>
2025-07-17 21:58:49 +0700

    2nd commit

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    text = models.CharField(max_length=300)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.text, self.published_date
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
+    text = models.CharField(max_length=300)
+    correct = models.BooleanField()
+
+    def __str__(self):
+        return self.text, self.correct
